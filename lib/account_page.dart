b143e1d4e1c13514a386fa9d44a39d0257b1b5c2
jack <jack68@naver.com>
2022-03-15 18:00:26 +0900

    detail fuction add

--- lib/account_page.dart
+++ lib/account_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AccountPage extends StatefulWidget {
   final User user;
@@ -14,6 +15,19 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   final GoogleSignIn _googleSignIn = GoogleSignIn();
+  int _postCount = 0;
+
+  @override
+  void initState(){
+    super.initState();
+    FirebaseFirestore.instance.collection('post').where('email',isEqualTo: widget.user.email)
+    .get().then((querySnapshot) => {
+      setState((){
+      _postCount = querySnapshot.docs.length;
+      }),
+    });
+    //_postCount =
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,9 +99,9 @@ class _AccountPageState extends State<AccountPage> {
               Text(widget.user.displayName!,style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),)
             ],
           ),
-          const Text('0\n게시물',textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0),),
-          const Text('0\n팔로워',textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0)),
-          const Text('0\n팔로잉',textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0)),
+          Text('$_postCount\n게시물',textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0),),
+          Text('0\n팔로워',textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0)),
+          Text('0\n팔로잉',textAlign: TextAlign.center,style: TextStyle(fontSize: 18.0)),
         ],
       ),
     );
